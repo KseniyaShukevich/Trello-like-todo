c9c93f378a5b2dd967ebbdc8432f669bc66533c6
Kseniya Shukevich <Kseniya.Shukevich@gmail.com>
2021-07-16 12:43:01 +0300

    feat: add material ui

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
+import 'normalize.css';
 import './scripts/index.tsx';
--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Button } from '@material-ui/core';
 
 const App: React.FC = () => (
-    <div> Hello</div>
+    <Button variant="contained" color="primary">Hello</Button>
 );
 
 ReactDom.render(<App/>, document.getElementById('root'));
